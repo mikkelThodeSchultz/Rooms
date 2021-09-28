2b98af5dab678a5e7da264530ef0f028de033c93
Horrid Entity <etienne.olieu@gmail.com>
2021-09-28 05:37:31 +0200

    Added flavor text

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -3,15 +3,15 @@ package com.company;
 
 public class Map {
 
-    Room room1 = new Room("room1", "description1");
-    Room room2 = new Room("room2", "description2");
-    Room room3 = new Room("room3", "description3");
-    Room room4 = new Room("room4", "description4");
-    Room room5 = new Room("room5", "description5");
-    Room room6 = new Room("room6", "description6");
-    Room room7 = new Room("room7", "description7");
-    Room room8 = new Room("room8", "description8");
-    Room room9 = new Room("room9", "description9");
+    Room room1 = new Room("Chute room", "There is a chute from the ceiling. It is slick with what you fear is blood. In the floor is a drain, equally sticky.");
+    Room room2 = new Room("Hook room", "All along the ceiling are sharp meat hooks as you would find at a butcher. Some of them are still dripping with fresh gore.");
+    Room room3 = new Room("Operating room", "In the center of this room is an operating table, it seems to have seen heavy use not too long ago.");
+    Room room4 = new Room("Kennel room", "Along the western wall are dog cages. All of the cages are empty, although some of them seem to have been broken open from the inside.");
+    Room room5 = new Room("Stair room", "A stair case leads up, a dim light shines above.");
+    Room room6 = new Room("Tool room", "The walls are lined with shelves and racks, filled with all manner of tools surgical and industrial.");
+    Room room7 = new Room("Storage room", "The room is filled with old furniture; chairs, tables and beds from countless different places.");
+    Room room8 = new Room("TV room", "An old TV facing a couch occupies the center of the room. An old sit-com is running of off an old VHS player.");
+    Room room9 = new Room("Grinder room", "A worryingly large industrial-grade meat-grinder fills the room.");
 
     public void createMap(){
         room1.setEast(room2);
--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -11,40 +11,50 @@ public class Parser {
         Player player = new Player("pede");
 
     public void runGame() {
+        System.out.println("You come to your senses... " +
+                "\nYour head feels as if you have been hit with a mallet. " +
+                "\nAs your eyes adjust to the surroundings, you realize that may just be the case. " +
+                "\nYour bare feet stick to the tiled floor and whatever half-dried liquid covers it, the scent of decay is cloying. " +
+                "\nYou think that it might be good idea to find an exit. " +
+                "\nFast." +
+                "\n(Type 'help' for a list of available commands)");
 
         while (goAgain) {
             String userInput = sc.nextLine();
             if (Objects.equals(userInput, "go north") || Objects.equals(userInput, "n") || Objects.equals(userInput, "north")) {
                 if (player.playerRoom.getNorth() != null) {
                     player.playerRoom = player.playerRoom.getNorth();
-                    System.out.println("you are in " + player.playerRoom.getName() + player.playerRoom.getDescription());
+                    System.out.println("You move north. You are now in the " + player.playerRoom.getName() + ". " + player.playerRoom.getDescription());
                 } else System.out.println("there is no door");
             } else if (Objects.equals(userInput, "go east") || Objects.equals(userInput, "e") || Objects.equals(userInput, "east")) {
                 if (player.playerRoom.getEast() != null) {
                     player.playerRoom = player.playerRoom.getEast();
-                    System.out.println("you are in " + player.playerRoom.getName() + player.playerRoom.getDescription());
+                    System.out.println("You move east. You are now in the " + player.playerRoom.getName() + ". " + player.playerRoom.getDescription());
                 } else System.out.println("there is no door");
             } else if (Objects.equals(userInput, "go south") || Objects.equals(userInput, "s") || Objects.equals(userInput, "south")) {
                 if (player.playerRoom.getSouth() != null) {
                     player.playerRoom = player.playerRoom.getSouth();
-                    System.out.println("you are in " + player.playerRoom.getName() + player.playerRoom.getDescription());
+                    System.out.println("You move south. You are now in the " + player.playerRoom.getName() + ". " + player.playerRoom.getDescription());
                 } else System.out.println("there is no door");
             } else if (Objects.equals(userInput, "go west") || Objects.equals(userInput, "w") || Objects.equals(userInput, "west")) {
                 if (player.playerRoom.getWest() != null) {
                     player.playerRoom = player.playerRoom.getWest();
-                    System.out.println("you are in " + player.playerRoom.getName() + player.playerRoom.getDescription());
+                    System.out.println("You move west. You are now in the " + player.playerRoom.getName() + ". " + player.playerRoom.getDescription());
                 } else System.out.println("there is no door");
             } else if (Objects.equals(userInput, "look")) {
-                System.out.println("looking around");
-                System.out.println("you are in " + player.playerRoom.getName() + player.playerRoom.getDescription());
+                System.out.println("You take a look at your surroundings.");
+                System.out.println("You are in the" + player.playerRoom.getName() + ". " + player.playerRoom.getDescription());
 
             } else if (Objects.equals(userInput, "exit")) {
                 System.out.println("Thank you for playing");
                 goAgain = false;
             } else if (Objects.equals(userInput, "help")) {
-                System.out.println("Helping info");
+                System.out.println("There is little help to be found here." +
+                        "\nType 'look' to get a description of your current room. " +
+                        "\nType '(n)orth', '(e)ast', '(s)outh', or '(w)est' to move in one of the cardinal directions." +
+                        "\nType 'exit' to quit the game.");
 
-            } else System.out.println("your choice was not registered please try again yo");
+            } else System.out.println("your choice was not registered please try a valid input.");
         }
     }
 }
