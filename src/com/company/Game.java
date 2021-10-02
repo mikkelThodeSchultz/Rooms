4a5bbfe6ca264351c3b7d4234758a161d72ea803
Horrid Entity <etienne.olieu@gmail.com>
2021-10-02 20:16:42 +0200

    Added parser method in Game class - needs revision

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -16,14 +16,15 @@ public class Game {
                 "\nYour head feels as if you have been hit with a mallet. " +
                 "\nAs your eyes adjust to the surroundings, you realize that may just be the case. " +
                 "\nYour bare feet stick to the tiled floor and whatever half-dried liquid covers it, the scent of decay is cloying. " +
-                "\nYou think that it might be good idea to find an exit. " +
+                "\nYou think that it might be a good idea to find an exit. " +
                 "\nFast." +
                 "\n(Type 'help' for a list of available commands)");
 
         while (goAgain) {
-            String userInput = sc.nextLine();
-            userInput.toLowerCase().trim();
-            String command = userInput.substring(0,1);//TODO piletasterne får programmet til at crashe her.
+            String userInput = sc.nextLine().toLowerCase().replaceAll("//s","");
+
+            String command = parser(userInput);
+            //String command = userInput.substring(0,1);//TODO piletasterne får programmet til at crashe her.
 
             switch (command) {
                 case "n":
@@ -104,4 +105,31 @@ public class Game {
                     printItems(temp);}
     }
 }
+
+    private String parser(String input){
+
+        String command = input+"zzzzzzzzzz";
+
+        if (command.substring(0,3).equals("go ")){
+            command = command.substring(3,4);
+        }
+        else if (command.substring(0,4).equals("exit")){
+            command = "q";
+        }
+        else if (command.substring(0,7).equals("pick up")){
+            command = command.substring(8);
+            //TODO metode til at håndtere identificering af ting man samler op.
+        }
+        else if (command.substring(0,4).equals("drop")){
+            command = command.substring(5);
+            //TODO metode til at identificere ting man vil smide.
+        }
+        else if (command.substring(0,1).equals(null)){
+            command = null;
+        } else
+            command = command.substring(0,1);
+
+        return command;
+    }
+
 }
